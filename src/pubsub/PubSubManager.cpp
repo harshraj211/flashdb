f4d54a42997579d2ac4ffa737906039640a156c7
harshraj211 <harshraj211@users.noreply.github.com>
2026-07-07 15:29:13 +0530

    pubsub: publish outside manager lock

--- src/pubsub/PubSubManager.cpp
+++ src/pubsub/PubSubManager.cpp
@@ -1,5 +1,7 @@
 #include "pubsub/PubSubManager.h"
 
+#include <vector>
+
 namespace flashdb {
 
 int PubSubManager::subscribe(const std::string& channel, int clientFd) {
@@ -37,18 +39,22 @@ int PubSubManager::unsubscribe(const std::string& channel, int clientFd) {
 
 int PubSubManager::publish(const std::string& channel, const std::string& message,
                            std::function<void(int fd, const std::string& msg)> writeFn) {
-    std::lock_guard<std::mutex> lock(mutex_);
+    std::vector<int> subscribers;
 
-    auto it = channels_.find(channel);
-    if (it == channels_.end()) {
-        return 0;
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it = channels_.find(channel);
+        if (it == channels_.end()) {
+            return 0;
+        }
+        subscribers.assign(it->second.begin(), it->second.end());
     }
 
     // Format the pub/sub message: "message <channel> <message>\n"
     std::string formatted = "message " + channel + " " + message + "\n";
 
     int notified = 0;
-    for (int fd : it->second) {
+    for (int fd : subscribers) {
         writeFn(fd, formatted);
         ++notified;
     }
